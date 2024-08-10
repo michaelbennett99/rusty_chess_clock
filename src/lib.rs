1dea80056ef3ef7b538a18a80f4d48480656763c
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-10 14:21:15 +0100

    export ClockState

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ mod duration_display;
 mod sleep;
 mod utils;
 
-pub use crate::clock::{Clock, ClockMode};
+pub use crate::clock::{Clock, ClockMode, ClockState};
 pub use crate::duration_display::DurationDisplay;
 pub use crate::sleep::Sleep;
