e86cadae511cb43c8d8c1f0d89660ec69c2df3f0
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-12 15:30:04 +0100

    finish basic chess clock functionality

--- src/chess_clock.rs
+++ src/chess_clock.rs
@@ -56,6 +56,13 @@ impl State {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Status {
+    Stopped,
+    Running,
+    Finished,
+}
+
 pub struct ChessClock {
     clocks: [Clock; 2],
     state: State,
@@ -87,6 +94,21 @@ impl ChessClock {
         )
     }
 
+    pub fn status(&mut self) -> Status {
+        let (t1, t2) = self.read();
+        let (s1, s2) = (
+            self.clocks[State::Player1.index()].state(),
+            self.clocks[State::Player2.index()].state()
+        );
+
+        match (t1.as_secs() * t2.as_secs(), s1, s2) {
+            (0, _, _) => Status::Finished,
+            (_, ClockState::Finished, ClockState::Finished) => Status::Finished,
+            (_, ClockState::Stopped, ClockState::Stopped) => Status::Stopped,
+            _ => Status::Running,
+        }
+    }
+
     fn start_current(&mut self) {
         self.clocks[self.state.index()].start();
     }
@@ -99,16 +121,20 @@ impl ChessClock {
         let current = self.state;
         let new = current.other();
 
-        if self.clocks[current.index()].state() == ClockState::Stopped {
+        if let ClockState::Running(_) = self.clocks[current.index()].state() {
             self.clocks[current.index()].stop();
             self.clocks[current.index()].add(self.rules.increment);
             self.clocks[new.index()].start();
-            self.state = new;
         }
+        self.state = new;
     }
 
     pub fn stop(&mut self) {
         self.clocks[self.state.index()].stop();
+    }
+
+    pub fn finish(&mut self) {
+        self.clocks.iter_mut().for_each(|clock| clock.finish());
     }
 }
 
@@ -116,7 +142,7 @@ impl Display for ChessClock {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let (p1, p2) = self.read();
         write!(
-            f, "Player 1: {}\t\tPlayer 2: {}",
+            f, "Player 1: {}    Player 2: {}",
             DurationDisplay::from(p1),
             DurationDisplay::from(p2)
         )
