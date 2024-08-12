9debc3ce0a0b91909c23dda0c8853980e7bdb9c8
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-12 15:31:16 +0100

    add ten minutes time constant

--- src/times.rs
+++ src/times.rs
@@ -3,3 +3,5 @@ use std::time::Duration;
 pub const ONE_SECOND: Duration = Duration::from_secs(1);
 pub const ONE_MINUTE: Duration = Duration::from_secs(60);
 pub const ONE_HOUR: Duration = Duration::from_secs(3600);
+
+pub const TEN_MINUTES: Duration = Duration::from_secs(10 * 60);
