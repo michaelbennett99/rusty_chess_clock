08609c3c8fc5cde31354c5e688d4b1bc70582c7c
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-12 16:37:43 +0100

    add 5 second time

--- src/times.rs
+++ src/times.rs
@@ -4,4 +4,5 @@ pub const ONE_SECOND: Duration = Duration::from_secs(1);
 pub const ONE_MINUTE: Duration = Duration::from_secs(60);
 pub const ONE_HOUR: Duration = Duration::from_secs(3600);
 
+pub const FIVE_SECONDS: Duration = Duration::from_secs(5);
 pub const TEN_MINUTES: Duration = Duration::from_secs(10 * 60);
