f76c7560a920a7fd00a08f6dfa23dd5cd417c9f0
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-12 12:24:47 +0100

    add start of chess clock

---
+++ src/chess_clock.rs
@@ -0,0 +1,124 @@
+use std::{fmt::Display, time::Duration};
+use crate::{Clock, ClockMode, ClockState, DurationDisplay};
+
+pub struct Rules {
+    player1_time: Duration,
+    player2_time: Duration,
+    increment: Duration,
+    starter: State,
+}
+
+impl Rules {
+    pub fn new(
+        player1_time: Duration, player2_time: Duration,
+        increment: Duration, starter: State
+    ) -> Self {
+        Self { player1_time, player2_time, increment, starter }
+    }
+}
+
+// #[derive(Debug, PartialEq)]
+// struct StoppedError(String);
+
+// impl Display for StoppedError {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(f, "{}", self.0)
+//     }
+// }
+
+// impl Error for StoppedError {}
+
+// impl StoppedError {
+//     fn new(msg: &str) -> Self {
+//         Self(msg.to_string())
+//     }
+// }
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum State {
+    Player1,
+    Player2,
+}
+
+impl State {
+    pub fn index(self) -> usize {
+        match self {
+            Self::Player1 => 0,
+            Self::Player2 => 1,
+        }
+    }
+
+    pub fn other(self) -> Self {
+        match self {
+            Self::Player1 => Self::Player2,
+            Self::Player2 => Self::Player1,
+        }
+    }
+}
+
+pub struct ChessClock {
+    clocks: [Clock; 2],
+    state: State,
+    rules: Rules,
+}
+
+impl ChessClock {
+    pub fn new(rules: Rules) -> Self {
+        Self {
+            clocks: [
+                Clock::new(
+                    ClockMode::CountDown,
+                    Some(rules.player1_time)
+                ),
+                Clock::new(
+                    ClockMode::CountDown,
+                    Some(rules.player2_time)
+                ),
+            ],
+            state: rules.starter,
+            rules,
+        }
+    }
+
+    pub fn read(&self) -> (Duration, Duration) {
+        (
+            self.clocks[State::Player1.index()].read(),
+            self.clocks[State::Player2.index()].read(),
+        )
+    }
+
+    fn start_current(&mut self) {
+        self.clocks[self.state.index()].start();
+    }
+
+    pub fn start(&mut self) {
+        self.start_current();
+    }
+
+    pub fn switch_player(&mut self) {
+        let current = self.state;
+        let new = current.other();
+
+        if self.clocks[current.index()].state() == ClockState::Stopped {
+            self.clocks[current.index()].stop();
+            self.clocks[current.index()].add(self.rules.increment);
+            self.clocks[new.index()].start();
+            self.state = new;
+        }
+    }
+
+    pub fn stop(&mut self) {
+        self.clocks[self.state.index()].stop();
+    }
+}
+
+impl Display for ChessClock {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let (p1, p2) = self.read();
+        write!(
+            f, "Player 1: {}\t\tPlayer 2: {}",
+            DurationDisplay::from(p1),
+            DurationDisplay::from(p2)
+        )
+    }
+}
