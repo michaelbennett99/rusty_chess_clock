ce7ef7219fd4c91a78bbcde9db2cd5a1307fd00d
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-12 16:40:19 +0100

    add update function separate from status function, add implementation of Color for Status, and macro for displaying player

--- src/chess_clock.rs
+++ src/chess_clock.rs
@@ -1,5 +1,6 @@
 use std::{fmt::Display, time::Duration};
 use crate::{Clock, ClockMode, ClockState, DurationDisplay};
+use termion::color::{self, Color};
 
 pub struct Rules {
     player1_time: Duration,
@@ -14,6 +15,18 @@ impl Rules {
         increment: Duration, starter: State
     ) -> Self {
         Self { player1_time, player2_time, increment, starter }
+    }
+
+    pub fn get_player1_time(&self) -> Duration {
+        self.player1_time
+    }
+
+    pub fn get_player2_time(&self) -> Duration {
+        self.player2_time
+    }
+
+    pub fn get_increment(&self) -> Duration {
+        self.increment
     }
 }
 
@@ -39,7 +52,7 @@ impl State {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Status {
     Stopped,
     Running,
@@ -77,7 +90,12 @@ impl ChessClock {
         )
     }
 
-    pub fn status(&mut self) -> Status {
+    pub fn update(&mut self) {
+        self.clocks.iter_mut()
+            .for_each(|clock| { clock.read_and_update(); });
+    }
+
+    pub fn status(&self) -> Status {
         let (t1, t2) = self.read();
         let (s1, s2) = (
             self.clocks[State::Player1.index()].state(),
@@ -121,13 +139,46 @@ impl ChessClock {
     }
 }
 
+impl Color for Status {
+    fn write_fg(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Running => color::Green.write_fg(f),
+            Self::Stopped => color::Yellow.write_fg(f),
+            Self::Finished => color::Red.write_fg(f),
+        }
+    }
+
+    fn write_bg(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Running => color::Green.write_bg(f),
+            Self::Stopped => color::Yellow.write_bg(f),
+            Self::Finished => color::Red.write_bg(f),
+        }
+    }
+}
+
 impl Display for ChessClock {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let (p1, p2) = self.read();
-        write!(
-            f, "Player 1: {}    Player 2: {}",
-            DurationDisplay::from(p1),
-            DurationDisplay::from(p2)
-        )
+
+        let fg = color::White;
+        let bg = self.status();
+
+        macro_rules! display_player {
+            ($player:expr, $time:expr, $label:expr) => {
+                if self.state == $player {
+                    write!(f, "{}{}", color::Fg(fg), color::Bg(bg))?;
+                }
+                write!(f, " {}: {} ", $label, DurationDisplay::from($time))?;
+                if self.state == $player {
+                    write!(f, "{}{}", color::Fg(color::Reset), color::Bg(color::Reset))?;
+                }
+            };
+        }
+
+        display_player!(State::Player1, p1, "Player 1");
+        display_player!(State::Player2, p2, "Player 2");
+
+        Ok(())
     }
 }
