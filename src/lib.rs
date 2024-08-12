eb963ece46ee897e24941ff8dea6d80a84b5986e
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-12 12:23:57 +0100

    add chess clock module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod clock;
+mod chess_clock;
 mod duration_display;
 mod sleep;
 pub mod times;
 pub mod utils;
 
 pub use crate::clock::{Clock, ClockMode, ClockState};
+pub use crate::chess_clock::{ChessClock, Rules, State};
 pub use crate::duration_display::DurationDisplay;
 pub use crate::sleep::Sleep;
