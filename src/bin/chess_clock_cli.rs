51d12fa0fa7e3b8a9c2bdcbd2f759cb6a3fb7116
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-08-13 11:24:08 +0100

    make function to read timing method

--- src/bin/chess_clock_cli.rs
+++ src/bin/chess_clock_cli.rs
@@ -1,15 +1,16 @@
 use std::{io::{self, Write}, thread::sleep, time::Duration};
 use rusty_chess_clock::{
-    times, Rules, State, ChessClock, Status, DurationDisplay
+    times, Rules, State, ChessClock, Status, DurationDisplay, TimingMethod
 };
 use termion::{clear, input::{TermRead, Keys}, raw::IntoRawMode, AsyncReader};
 
 fn main() {
     let start_time = get_start_time();
     let increment = get_increment();
+    let timing_method = get_timing_method();
     let rules = Rules::new(
         start_time, start_time,
-        increment, State::Player1
+        increment, State::Player1, timing_method
     );
     print_instructions(&rules);
 
@@ -43,6 +44,28 @@ fn get_increment() -> Duration {
     }
 }
 
+fn get_timing_method() -> TimingMethod {
+    loop {
+        print!(
+            "\r{}Timing method (f for Fischer, b for Bronstein): ",
+            clear::CurrentLine
+        );
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {},
+            Err(_) => continue,
+        }
+
+        match input.trim() {
+            "f" => return TimingMethod::Fischer,
+            "b" => return TimingMethod::Bronstein,
+            _ => continue,
+        }
+    }
+}
+
 fn print_instructions(rules: &Rules) {
     println!("===================== Chess Clock ====================");
     println!(
@@ -51,7 +74,7 @@ fn print_instructions(rules: &Rules) {
         DurationDisplay::from(rules.get_player2_time())
     );
     println!("Extra time: {}", DurationDisplay::from(rules.get_increment()));
-    println!("Timing Method: Fischer");
+    println!("Timing Method: {:?}", rules.get_timing_method());
     println!("Instructions:");
     println!("- Active player is indicated by highlighted background");
     println!("- Yellow: Stopped, Green: Running, Red: Finished");
